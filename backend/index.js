eada0a211ae43a7c147f37d68d10ef95e0a9060b
kunjabiharibagh <kbbagh2005@github.com>
2026-06-22 13:59:02 +0530

    use env var for CORS origin

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ connectionDB();
 const app=express();
 app.use(express.json());//json data
 app.use(cors({
-  origin: [process.env.PORT, 'http://localhost:5173'],
+  origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
   credentials: true
 }));
 app.use('/api/messages',messageRoutes);
